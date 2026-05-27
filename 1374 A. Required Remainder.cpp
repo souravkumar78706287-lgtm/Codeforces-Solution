2244eb780d5d49e787583ef7ba4f6d660c960912
sourav kumar <souravkumar78706287@gmail.com>
2026-05-28 00:42:47 +0530

    Update 1374 A. Required Remainder.cpp

--- 1374 A. Required Remainder.cpp
+++ 1374 A. Required Remainder.cpp
@@ -1,19 +1,13 @@
-//Bismillahir Rahmanir Rahim
 #include<bits/stdc++.h>
-#define ll              long long
-#define ull             unsigned long long
-#define pb              push_back
-#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
 using namespace std;
 int main()
 {
-    fastread();
-    ll t;
-    ll x,y,n;
+    long long t;
+    long long x,y,n;
     cin>>t;
     while(t--){
         cin>>x>>y>>n;
-        ll ans = 0;
+        long long ans = 0;
         ans = n - n % x + y;
         if(ans <= n){
             cout<<ans<<endl;
