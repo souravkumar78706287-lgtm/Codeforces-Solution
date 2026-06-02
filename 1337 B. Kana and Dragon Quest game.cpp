e07c0e6ee9681f85d8fd051adbf08e9aa2fe1d4a
sourav kumar <souravkumar78706287@gmail.com>
2026-06-02 17:40:21 +0530

    Update 1337 B. Kana and Dragon Quest game.cpp

--- 1337 B. Kana and Dragon Quest game.cpp
+++ 1337 B. Kana and Dragon Quest game.cpp
@@ -1,53 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main(){
     int t;
-    long long int x,n,m,N,M;
-    long long int ans,ans1;
     cin>>t;
-    while(t--)
-    {
-        ans=ans1=0;
-        int tag=0;
+    while(t--){
+        int x,n,m;
         cin>>x>>n>>m;
-
-        N=n;
-        M=m;
-        ans=ans1=x;
-        while(n-- && ans>0){
-                ans=(ans/2)+10;
-                if(ans<=0){
-                    tag=1;
-                    break;
-                }
-            }
-
-        while(m-- && ans>0){
-                ans=ans-10;
-                if(ans<=0){
-                    tag=1;
-                    break;
-                }
-            }
-        while(M-- && ans1>0){
-                ans1=ans1-10;
-                if(ans1<=0){
-                    tag=1;
-                    break;
-                }
-            }
-        while(N-- && ans1>0){
-                ans1=(ans1/2)+10;
-                if(ans1<=0){
-                    tag=1;
-                    break;
-                }
-            }
-        if(tag==1){
+        int temp=0;
+        while(n--&& x>20){
+            x=x/2+10;
+        }
+        x-=10*m;
+        if(x<=0){
             cout<<"YES"<<endl;
         }
-        else
+        else{
             cout<<"NO"<<endl;
+        }
     }
+    return 0;
 }
